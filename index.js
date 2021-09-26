01e9b3383270ad3a6ba05ea2a648198fd2b96576
KariUlrich <kariulrich@hotmail.com>
2021-09-26 17:58:38 -0300

    terminando modales

--- index.js
+++ index.js
@@ -2,37 +2,52 @@
 const botonBalance = document.querySelector(".botonBalance")
 const botonCategorias = document.querySelector(".botonCategorias")
 const botonReportes = document.querySelector(".botonReportes")
-const divPrincipal = document.querySelector(".divPrincipal")
+const secPrincipal = document.querySelector(".secPrincipal")
 const categorias = document.querySelector(".categorias")
 const reportes = document.querySelector(".reportes")
+const botonNuevaOpe = document.querySelector(".botonNuevaOpe")
+const nuevaOperacion = document.querySelector(".nuevaOperacion")
 
 botonBalance.onclick = () => {
-    divPrincipal.classList.remove("is-hidden")
+    secPrincipal.classList.remove("is-hidden")
     categorias.classList.add("is-hidden")
     reportes.classList.add("is-hidden")
     nuevaOperacion.classList.add("is-hidden")
 }
 
 botonCategorias.onclick = () => {
-    divPrincipal.classList.add("is-hidden")
+    secPrincipal.classList.add("is-hidden")
     categorias.classList.remove("is-hidden")
     reportes.classList.add("is-hidden")
     nuevaOperacion.classList.add("is-hidden")
 }
 
 botonReportes.onclick = () => {
-    divPrincipal.classList.add("is-hidden")
+    secPrincipal.classList.add("is-hidden")
     categorias.classList.add("is-hidden")
     reportes.classList.remove("is-hidden")
     nuevaOperacion.classList.add("is-hidden")
 }
 
-const botonNuevaOpe = document.querySelector(".botonNuevaOpe")
-const nuevaOperacion = document.querySelector(".nuevaOperacion")
-
 botonNuevaOpe.onclick = () => {
     nuevaOperacion.classList.remove("is-hidden")
-    divPrincipal.classList.add("is-hidden")
+    secPrincipal.classList.add("is-hidden")
 }
+
+const divFiltros = document.querySelector(".divFiltros")
+const botonOcultarFiltros = document.querySelector(".botonOcultarFiltros")
+
+const ocultarFiltros = () => {
+    if (botonOcultarFiltros.textContent === "Ocultar Filtros"){
+        divFiltros.classList.add("is-hidden")
+        botonOcultarFiltros.textContent = "Mostrar Filtros"
+    }
+    else{
+        divFiltros.classList.remove("is-hidden")
+        botonOcultarFiltros.textContent = "Ocultar Filtros"
+    }
+}
+
+botonOcultarFiltros.onclick = ocultarFiltros
 
 
